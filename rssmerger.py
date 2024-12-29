e6fea95270f10f22aa8fa74dd4378cac27b257c5
csredkey <cyrus@redkeyhq.com>
2024-12-28 19:54:33 -0500

    AWS S3 Action added

--- rssmerger.py
+++ rssmerger.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import os
-import dotenv import load_dotenv
+from dotenv import load_dotenv
 import feedparser
 import time
 from feedgen.feed import FeedGenerator
@@ -13,27 +13,25 @@ sources = [
   'https://blog.qualys.com/feed',
   'https://msrc-blog.microsoft.com/feed/',
   'https://www.microsoft.com/security/blog/feed/',
-  'https://googleprojectzero.blogspot.com/feeds/posts/default',
   'http://feeds.feedburner.com/fortinet/blog/threat-research',
   'https://www.crowdstrike.com/blog/category/threat-intel-research/',
   'https://blog.cloudflare.com/tag/security/rss',
   'https://research.checkpoint.com/feed/',
   'https://www.wired.com/feed/category/security/latest/rss',
-  'http://www.techrepublic.com/rssfeeds/topic/security/?feedType=rssfeeds',
+  'https://www.techrepublic.com/rssfeeds/white-papers/',
+  'https://www.techrepublic.com/rssfeeds/topic/cloud-security/',
+  'https://www.techrepublic.com/rssfeeds/topic/artificial-intelligence/',
+  'https://www.techrepublic.com/rssfeeds/topic/cybersecurity/',
+  'https://www.techrepublic.com/rssfeeds/topic/startups/',
   'https://www.schneier.com/blog/atom.xml',
   'http://krebsonsecurity.com/feed/',
   'https://www.hackmageddon.com/feed/',
   'https://www.bleepingcomputer.com/feed',
   'https://arstechnica.com/tag/security/feed/',
   'https://www.nist.gov/blogs/cybersecurity-insights/rss.xml',
-  'https://us-cert.cisa.gov/ics/advisories/advisories.xml',
   'https://isc.sans.edu/rssfeed_full.xml',
   'https://us-cert.cisa.gov/ncas/analysis-reports.xml',
-  'https://us-cert.cisa.gov/ncas/alerts.xml',
-  'https://us-cert.cisa.gov/ics/advisories/advisories.xml',
   'https://us-cert.cisa.gov/ncas/current-activity.xml',
-  'https://www.reddit.com/r/cybersecurity/.rss',
-  'http://www.reddit.com/r/netsec/.rss',
   'http://www.darknet.org.uk/feed/',
   'https://medium.com/feed/@sebdraven',
   'https://www.bsi.bund.de/SiteGlobals/Functions/RSSFeed/RSSNewsfeed/RSSNewsfeed.xml',
@@ -51,7 +49,7 @@ for url in sources:
     fullList.append(item)
 
 fullList.sort(key=lambda item: item['updated_parsed'], reverse=True)
-outList = fullList [0:15]
+outList = fullList [0:500]
 
 load_dotenv()
 name = os.getenv('NAME')
@@ -71,7 +69,8 @@ for item in outList:
   fe.id(item['id'])
   fe.title(item['title'])
   fe.link(href=item['link'])
-  fe.updated(item['updated'])
+  if('updated' in item):
+    fe.updated(item['updated'])
   # add author if present in original
   if('author' in item):
     fe.author(name=item['author'])
@@ -79,7 +78,12 @@ for item in outList:
     # If the summary contains HTML code, set its type.
     summary_type = 'html' if '<' in item['summary'] else None
     fe.summary(summary=item['summary'], type=summary_type)
-  for tag in item.tags:
-    fe.category(term=tag.term)
-  for content in item.content:
-    fe.content(content=content.value, type='html')
+  if 'tags' in item:
+    for tag in item.tags:
+      fe.category(term=tag.term)
+  if 'content' in item:
+    for content in item.content:
+      fe.content(content=content.value, type='html')
+
+fg.rss_str(pretty=True)
+fg.rss_file('feed.xml')
